addf1dcf0d4420fa833435d8d51cd3c3288cb337
leonsxia <leon_s_xia@126.com>
2025-07-02 17:09:30 +0800

    [Creature] reset helpers when melee is over

--- src/world/components/composite/creatures/CreatureBase.js
+++ src/world/components/composite/creatures/CreatureBase.js
@@ -286,7 +286,7 @@ class CreatureBase extends CustomizedCreatureTofu {
 
                 const endCallback = () => {
 
-                    super.melee(false);
+                    this.melee(false);
 
                 }
 
